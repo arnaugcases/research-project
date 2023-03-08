5bdec1c2d5e1a64221bd46afb517120cbe5c1194
Arnau Garcia Cases <60266982+arnaugcases@users.noreply.github.com>
2023-03-08 18:29:25 +0100

    Contract modified and reduced data set added
    A reduced data set added. In this case there are only 10 aircraft during 50 epoch values. Each aircraft is flying and none of the fields is empty.

--- contracts/AircraftDatabase.sol
+++ contracts/AircraftDatabase.sol
@@ -8,12 +8,6 @@ contract AircraftDatabase {
             Public Variables 
     ---------------------------------
     */
-    // Array storing all the timestamps for which aircraft data is available
-    uint32[] public epochList;
-
-    // List of aircraft Id in each epoch
-    mapping(uint32 => bytes6[]) public aircraftInEpoch;
-
     // Most recent information about an aircraft
     struct AircraftStateVector {
         int256 longitude;
@@ -26,7 +20,14 @@ contract AircraftDatabase {
         uint32 timestamp;
     }
 
+    // Structure mapping of a given Aircraft ICAO24 identifier
     mapping(bytes6 => AircraftStateVector) public aircraftInfo;
+
+    // Array storing all the aircraft icao24 identifier for which the smart contract has data
+    bytes6[] public aircraftList;
+
+    // Mapping to check if there is information on an aircraft
+    mapping(bytes6 => bool) public isAircraftInfoAvailable;
 
     // List of contributors that submitted information for aircraft
     address[] public listOfContributors;
@@ -37,20 +38,7 @@ contract AircraftDatabase {
     // Reputation scores (0-100) for each contributor
     mapping(address => uint8) public reputationScore;
 
-    /* 
-    ---------------------------------
-            Private Variables 
-    ---------------------------------
-    */
-
-    // Mapping that relates each icao24 identifier to a mapping of epochs and their associated data
-    //mapping(bytes6 => mapping(uint256 => Aircraft)) public aircraftData;
-
-    // Mapping that relates each icao24 identifier to an array of epochs for which data is available
-    mapping(bytes6 => uint256[]) public aircraftEpochs;
-
-    // Array storing all the aircraft icao24 identifier for which the smart contract has data
-    bytes6[] public aircraftList;
+    // Structure to store the new values for a given aircraft
 
     // Function for adding aircraft data for a specific epoch
     function submitAircraftData(
@@ -65,49 +53,26 @@ contract AircraftDatabase {
         int16 _verticalRate
     ) public {
         // Add the specified aircraft data to the aircraftData mapping for the specified epoch
-        aircraftData[_icao24][_epoch] = Aircraft({
-            longitude: _longitude,
-            latitude: _latitude,
-            geoAltitude: _geoAltitude,
-            onGround: _onGround,
-            velocity: _velocity,
-            trueTrack: _trueTrack,
-            verticalRate: _verticalRate
-        });
 
         // If the aircraft is not already in the aircraftList array, add it
-        if (!containsAircraft(_icao24)) {
+        if (!isAircraftInfoAvailable[_icao24]) {
             aircraftList.push(_icao24);
         }
 
-        // If the epoch is not already in the aircraftEpochs mapping for the specified aircraft, add it
-        if (!containsEpoch(_icao24, _epoch)) {
-            aircraftEpochs[_icao24].push(_epoch);
+        // Add the sender to the list of contributors
+        if (!addressContributed[msg.sender]) {
+            listOfContributors.push(msg.sender);
         }
     }
 
-    // Function to check if an aircraft is already in the aircraftList array
-    function containsAircraft(bytes6 _icao24) public view returns (bool) {
-        for (uint256 i = 0; i < aircraftList.length; i++) {
-            if (aircraftList[i] == _icao24) {
-                return true;
-            }
-        }
-        return false;
+    /* 
+        VIEW functions (get data)
+    */
+    function getAircraftList() public view returns (bytes6[] memory) {
+        return aircraftList;
     }
 
-    // Function to check if an epoch is already in the aircraftEpochs mapping for the specified aircraft
-    function containsEpoch(bytes6 _icao24, uint256 _epoch)
-        public
-        view
-        returns (bool)
-    {
-        uint256[] memory epochs = aircraftEpochs[_icao24];
-        for (uint256 i = 0; i < epochs.length; i++) {
-            if (epochs[i] == _epoch) {
-                return true;
-            }
-        }
-        return false;
+    function getContributorList() public view returns (address[] memory) {
+        return listOfContributors;
     }
 }
