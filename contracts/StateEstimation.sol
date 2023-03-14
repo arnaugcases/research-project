03d821c7e76b548c6b1272987481de0e5dead71e
tlipavsky <tlipavsky@yahoo.co.uk>
2023-03-14 17:24:12 +0100

    Enhanced state estimation added using longitude and latitude prediction

--- contracts/StateEstimation.sol
+++ contracts/StateEstimation.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "./AircraftDatabase.sol";
 
+import "./Trigonometry.sol";
+
 library StateEstimation {
     function computeEstimates(
         AircraftDatabase.AircraftStateOccurrences[] memory observations,
@@ -132,6 +134,69 @@ function observationError(AircraftDatabase.AircraftStateVector[] memory observat
     }
 
     return _final_error;
+}
+
+function trueValueFromObservationsEnhanced(AircraftDatabase.AircraftStateOccurrences[] memory observations, uint32 currentEpoch, AircraftDatabase.AircraftStateVector memory previous_val ) public pure returns (AircraftDatabase.AircraftStateVector memory){
+
+    AircraftDatabase.AircraftStateVector memory obs_val_current = computeEstimates(observations, currentEpoch);
+    
+    // Prediction part next
+
+    AircraftDatabase.AircraftStateVector memory trueValue = obs_val_current;
+
+    int24 delta_t = int24(uint24((obs_val_current.timestamp > previous_val.timestamp) ? obs_val_current.timestamp - previous_val.timestamp : previous_val.timestamp  - obs_val_current.timestamp));
+
+    if (delta_t>900) {
+        return trueValue;
+    }
+
+    int24[] memory prediction = position_prediction(delta_t, previous_val); //[0] - longitude, [1] - latitude
+/*
+	Calculating prediction validity:
+	Assuming planes fly in straight lines (strong assumption I know), the velocity 
+	and trueTrack should be the same in obs1 and obs2 - or at least close to
+	each other, given the time interval between observations is small. Taking the 
+	values of obs 2 (more recent) as 'true' values, the difference 
+	between these values and those in in obs1 (older) determined the accuracy/
+	validity of the prediction components. 
+*/
+    int24 velocity_offset = previous_val.velocity > obs_val_current.velocity ? (previous_val.velocity - obs_val_current.velocity)*1000/previous_val.velocity :
+    (obs_val_current.velocity - previous_val.velocity)*1000/obs_val_current.velocity;
+
+    int24 track_offset = previous_val.trueTrack > obs_val_current.trueTrack ? (previous_val.trueTrack - obs_val_current.trueTrack)*1000/previous_val.trueTrack :
+    (obs_val_current.trueTrack - previous_val.trueTrack)*1000/obs_val_current.trueTrack;
+    //offset value 0-1000 
+
+
+    int gain = 2000-int(track_offset)-int(velocity_offset);
+
+    trueValue.longitude = int24((2000*int(obs_val_current.longitude) + int(prediction[0])*gain)/(2000+gain));
+    trueValue.latitude = int24((2000*int(obs_val_current.latitude) + int(prediction[1]*gain))/(2000+gain));
+
+
+   return trueValue;
+
+}
+
+function position_prediction(int24 _delta_time, AircraftDatabase.AircraftStateVector memory previous_estimate) pure internal returns(int24[] memory){
+
+    int24  Re = 6371; //km
+
+    int24[] memory data = new int24[](2);
+
+    //altitude assumed in metres, velocity ms-1, delta_t seconds, result - delta_hor - is in km*1000
+    
+    //trigonometry factor *100, scale factor *10. Pi*1000 -> resulting angle in deg*10000
+
+    int24  delta_long = int24(int(Trigonometry.sin(uint16(int16(previous_estimate.trueTrack)))/328*previous_estimate.velocity*_delta_time/1000
+    *(Re*10/(Re+int24(previous_estimate.geoAltitude)/1000)))*10000/(2*int(Re)*3142)*360);
+	int24  delta_lat = int24(int(Trigonometry.cos(uint16(int16(previous_estimate.trueTrack)))/328*previous_estimate.velocity*_delta_time/1000*
+    (Re*10/(Re+int24(previous_estimate.geoAltitude)/1000)))*10000/(2*int(Re)*3142)*360);
+	
+	data[0] = previous_estimate.longitude+delta_long; //assumes angle in deg*10000
+    data[1] = previous_estimate.latitude+delta_lat;
+
+    return data;
 }
 
 }
